8c41098c237946109e46302e8d8fb66e6296a41c
EkkoG <beijiu572@gmail.com>
2023-01-01 14:30:47 +0800

    Make ApplyStyleKit usage like EGChainable

--- DeclarativeUIKit/Classes/ApplyStyleKit/ApplyStyle+.swift
+++ DeclarativeUIKit/Classes/ApplyStyleKit/ApplyStyle+.swift
@@ -5,3 +5,11 @@ extension ApplyStyleProtocol {
         return applyStyle
     }
 }
+
+extension StyleObject: BodyBuilderItemable where Base: UIView {
+    public var bodyBuilderItem: BodyBuilderItem { .single(self.base) }
+
+    public func asView() -> UIView {
+        return self.base
+    }
+}
